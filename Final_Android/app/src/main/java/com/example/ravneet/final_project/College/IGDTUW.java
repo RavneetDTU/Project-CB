c4c57ee35fe82c79741be11c027b697f540032bd
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-07-08 22:30:47 +0530

    College List Created

---
+++ Final_Android/app/src/main/java/com/example/ravneet/final_project/College/IGDTUW.java
@@ -0,0 +1,15 @@
+package com.example.ravneet.final_project.College;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ravneet.final_project.R;
+
+public class IGDTUW extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_igdtuw);
+    }
+}
