7ea757bb1f9481fea1f3d17da8ac5c14e079c183
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-07-11 10:44:20 +0530

    Show

--- Final_Android/app/src/main/java/com/example/ravneet/final_project/College/DTU.java
+++ Final_Android/app/src/main/java/com/example/ravneet/final_project/College/DTU.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.ravneet.final_project.Adapter.BookAdapter;
 import com.example.ravneet.final_project.BookPostActivity;
+import com.example.ravneet.final_project.Interface.OnItemClickListner;
+import com.example.ravneet.final_project.Model.Book;
 import com.example.ravneet.final_project.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,11 +24,20 @@ import com.google.firebase.database.ValueEventListener;
 
 public class DTU extends AppCompatActivity {
 
+    public static final String TAG = "false";
+
     RecyclerView rv_dtu;
     Button postAdd;
 
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
+    ChildEventListener childEventListener;
+
+    BookAdapter bookAdapter;
+
+    private OnItemClickListner onItemClickListner;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +56,44 @@ public class DTU extends AppCompatActivity {
         });
 
         firebaseDatabase = FirebaseDatabase.getInstance();
-        databaseReference = firebaseDatabase.getReferenceFromUrl("https://finalandroid-2774c.firebaseio.com/");
+        databaseReference = firebaseDatabase.getReference();
+
+        childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Book thisbook = dataSnapshot.getValue(Book.class);
+                String bkname = thisbook.getBookname();
+                String sellername = thisbook.getSellername();
+                String price = thisbook.getPrice();
+                String mbnumber = thisbook.getMobilenumber();
+                Log.d(TAG, "onChildAdded: "+bkname+sellername+price+mbnumber);
+
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+        databaseReference.addChildEventListener(childEventListener);
+
     }
 
 }
